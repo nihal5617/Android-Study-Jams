d9ecd209d2e6fcb6eea69fe8e660f9d1f9541eb6
Nihal Vishal Gupta <nihalng786@gmail.com>
2022-01-12 14:21:09 +0530

    Splash bug solved

--- app/src/main/java/com/example/storyapp/SplashActivity.kt
+++ app/src/main/java/com/example/storyapp/SplashActivity.kt
@@ -11,7 +11,8 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+            this.startActivity(Intent(this,MainActivity::class.java))
+            this.finish()
         },2000)
     }
 }
